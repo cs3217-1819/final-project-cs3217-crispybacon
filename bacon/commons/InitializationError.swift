0ace8f64652e94e2b7bae9bd5a5234921f9ba4b6
Fabian <fabianterh@gmail.com>
2019-03-19 17:22:23 +0800

    Add InitializationError into "commons" group

---
+++ bacon/commons/InitializationError.swift
@@ -0,0 +1,13 @@
+//
+//  InitializationError.swift
+//  bacon
+//
+//  Created by Fabian Terh on 19/3/19.
+//  Copyright © 2019 nus.CS3217. All rights reserved.
+//
+
+import Foundation
+
+struct InitializationError: Error {
+    let message: String
+}
