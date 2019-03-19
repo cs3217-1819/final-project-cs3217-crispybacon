92771926ee523ca6889bfad956c4b0468432ea54
Fabian <fabianterh@gmail.com>
2019-03-19 13:32:06 +0800

    Create empty project

---
+++ bacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bacon
+//
+//  Created by Fabian Terh on 19/3/19.
+//  Copyright © 2019 nus.CS3217. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
