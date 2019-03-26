250d1d25534176546dabc4a4a6c5f66683df688f
Fabian <fabianterh@gmail.com>
2019-03-26 18:56:02 +0800

    Add tests for TransactionTime

---
+++ baconTests/TransactionTimeTests.swift
@@ -0,0 +1,28 @@
+
+//
+//  TransactionTimeTests.swift
+//  baconTests
+//
+//  Created by Fabian Terh on 26/3/19.
+//  Copyright © 2019 nus.CS3217. All rights reserved.
+//
+
+import XCTest
+@testable import bacon
+
+class TransactionTimeTests: XCTestCase {
+
+    func test_currentDate() {
+        let date = Date()
+        let transactionTime = TransactionTime(date)
+
+        let calendar = Calendar.current
+        XCTAssertEqual(transactionTime.year, calendar.component(.year, from: date))
+        XCTAssertEqual(transactionTime.month, calendar.component(.month, from: date))
+        XCTAssertEqual(transactionTime.day, calendar.component(.day, from: date))
+        XCTAssertEqual(transactionTime.hour, calendar.component(.hour, from: date))
+        XCTAssertEqual(transactionTime.minute, calendar.component(.minute, from: date))
+        XCTAssertEqual(transactionTime.second, calendar.component(.second, from: date))
+    }
+
+}
