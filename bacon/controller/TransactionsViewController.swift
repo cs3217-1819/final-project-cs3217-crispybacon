2b1309a49d0a8f9e4f90582b2d769306481a4c20
kumuwu <e0203066@u.nus.edu>
2019-03-27 23:14:09 +0800

    now this crazy library works

--- bacon/controller/TransactionsViewController.swift
+++ bacon/controller/TransactionsViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class TransactionsViewController: UIViewController {
     
-    // hard coded
-    var cellHeights = (0..<100).map { _ in C.CellHeight.close }
+    enum Const {
+        static let closeCellHeight: CGFloat = 179
+        static let openCellHeight: CGFloat = 488
+        static let rowsCount = 10
+    }
+    
+    var cellHeights: [CGFloat] = []
     
     @IBOutlet private weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setup()
         // Do any additional setup after loading the view.
     }
+    private func setup() {
+        cellHeights = Array(repeating: Const.closeCellHeight, count: Const.rowsCount)
+        tableView.estimatedRowHeight = Const.closeCellHeight
+        tableView.rowHeight = UITableView.automaticDimension
+        //tableView.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "background"))
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = UIRefreshControl()
+            tableView.refreshControl?.addTarget(self, action: #selector(refreshHandler), for: .valueChanged)
+        }
+    }
+    
+    @objc func refreshHandler() {
+        let deadlineTime = DispatchTime.now() + .seconds(1)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: { [weak self] in
+            if #available(iOS 10.0, *) {
+                self?.tableView.refreshControl?.endRefreshing()
+            }
+            self?.tableView.reloadData()
+        })
+    }
+}
 
     /*
     // MARK: - Navigation
@@ -31,60 +57,71 @@ class TransactionsViewController: UIViewController {
     }
     */
 
-}
+
 
 extension TransactionsViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return 10
     }
-
+    
+    func tableView(_: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard case let cell as TransactionCell = cell else {
+            return
+        }
+        
+        cell.backgroundColor = .clear
+        
+        if cellHeights[indexPath.row] == Const.closeCellHeight {
+            cell.unfold(false, animated: false, completion: nil)
+        } else {
+            cell.unfold(true, animated: false, completion: nil)
+        }
+        
+        //cell.number = indexPath.row
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let rawCell = tableView.dequeueReusableCell(withIdentifier: "transactionCell", for: indexPath)
+        guard let cell = rawCell as? FoldingCell else {
+            return rawCell
+        }
+        let durations: [TimeInterval] = [0.26, 0.2, 0.2]
+        cell.durationsForExpandedState = durations
+        cell.durationsForCollapsedState = durations
         return cell
     }
     
-    fileprivate struct C {
-        struct CellHeight {
-            static let close: CGFloat = 75 // equal or greater foregroundView height
-            static let open: CGFloat = 456 // equal or greater containerView height
-        }
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return cellHeights[indexPath.row]
     }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard case let cell as FoldingCell = tableView.cellForRow(at: indexPath) else {
+        
+        let rawCell = tableView.cellForRow(at: indexPath)
+        guard let cell = rawCell as? FoldingCell else {
+            return
+        }
+        
+        if cell.isAnimating() {
             return
         }
         
         var duration = 0.0
-        if cell.isUnfolded { // it was cellIsCollapsed
-            cellHeights[indexPath.row] = C.CellHeight.open
+        let cellIsCollapsed = cellHeights[indexPath.row] == Const.closeCellHeight
+        if cellIsCollapsed {
+            cellHeights[indexPath.row] = Const.openCellHeight
             cell.unfold(true, animated: true, completion: nil)
             duration = 0.5
         } else {
-            cellHeights[indexPath.row] = C.CellHeight.close
+            cellHeights[indexPath.row] = Const.closeCellHeight
             cell.unfold(false, animated: true, completion: nil)
             duration = 0.8
         }
         
-        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: {
+        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: { () -> Void in
             tableView.beginUpdates()
             tableView.endUpdates()
-            return
         }, completion: nil)
     }
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        
-        if case let cell as FoldingCell = cell {
-            if cellHeights[indexPath.row] == C.CellHeight.close {
-                cell.unfold(false)
-                //cell.selectedAnimation(false, animated: false, completion:nil)
-            } else {
-                cell.unfold(true)
-                //foldingCell.selectedAnimation(true, animated: false, completion: nil)
-            }
-        }
-    }
+
 }
