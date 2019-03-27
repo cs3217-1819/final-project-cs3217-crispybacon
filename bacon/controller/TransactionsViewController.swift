59d75318e467ae7699fe15451f9349949666eaf0
kumuwu <e0203066@u.nus.edu>
2019-03-27 23:28:53 +0800

    clean up

--- bacon/controller/TransactionsViewController.swift
+++ bacon/controller/TransactionsViewController.swift
@@ -9,21 +9,20 @@
 import UIKit
 
 class TransactionsViewController: UIViewController {
-    
+
     enum Const {
         static let closeCellHeight: CGFloat = 179
         static let openCellHeight: CGFloat = 488
         static let rowsCount = 10
     }
-    
+
     var cellHeights: [CGFloat] = []
-    
-    @IBOutlet private weak var tableView: UITableView!
+
+    @IBOutlet private weak var tableView: UITableView! // not being used yet
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        // Do any additional setup after loading the view.
     }
     private func setup() {
         cellHeights = Array(repeating: Const.closeCellHeight, count: Const.rowsCount)
@@ -35,51 +34,39 @@ class TransactionsViewController: UIViewController {
             tableView.refreshControl?.addTarget(self, action: #selector(refreshHandler), for: .valueChanged)
         }
     }
-    
+
     @objc func refreshHandler() {
         let deadlineTime = DispatchTime.now() + .seconds(1)
-        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: { [weak self] in
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime) { [weak self] in
             if #available(iOS 10.0, *) {
                 self?.tableView.refreshControl?.endRefreshing()
             }
             self?.tableView.reloadData()
-        })
+        }
     }
 }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
 
 extension TransactionsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
         return 10
     }
-    
+
     func tableView(_: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         guard case let cell as TransactionCell = cell else {
             return
         }
-        
+
         cell.backgroundColor = .clear
-        
+
         if cellHeights[indexPath.row] == Const.closeCellHeight {
             cell.unfold(false, animated: false, completion: nil)
         } else {
             cell.unfold(true, animated: false, completion: nil)
         }
-        
+
         //cell.number = indexPath.row
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let rawCell = tableView.dequeueReusableCell(withIdentifier: "transactionCell", for: indexPath)
         guard let cell = rawCell as? FoldingCell else {
@@ -90,22 +77,22 @@ extension TransactionsViewController: UITableViewDataSource, UITableViewDelegate
         cell.durationsForCollapsedState = durations
         return cell
     }
-    
+
     func tableView(_: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return cellHeights[indexPath.row]
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+
         let rawCell = tableView.cellForRow(at: indexPath)
         guard let cell = rawCell as? FoldingCell else {
             return
         }
-        
+
         if cell.isAnimating() {
             return
         }
-        
+
         var duration = 0.0
         let cellIsCollapsed = cellHeights[indexPath.row] == Const.closeCellHeight
         if cellIsCollapsed {
@@ -117,7 +104,7 @@ extension TransactionsViewController: UITableViewDataSource, UITableViewDelegate
             cell.unfold(false, animated: true, completion: nil)
             duration = 0.8
         }
-        
+
         UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: { () -> Void in
             tableView.beginUpdates()
             tableView.endUpdates()
--- bacon/view/TransactionCell.swift
+++ bacon/view/TransactionCell.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 class TransactionCell: FoldingCell {
-    override func animationDuration(_ itemIndex:NSInteger, type:AnimationType)-> TimeInterval {
-        
+    override func animationDuration(_ itemIndex: NSInteger, type: AnimationType) -> TimeInterval {
+
         // durations count equal it itemCount
         let durations = [0.33, 0.26, 0.26] // timing animation for each view
         return durations[itemIndex]
