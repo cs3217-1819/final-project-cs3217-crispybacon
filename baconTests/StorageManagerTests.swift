3293b0f62eb29eb90d30ef229833336e062bd41b
Travis Ching Jia Yea <travisc@Traviss-iMac.local>
2019-03-27 17:27:56 +0800

    Implement StorageManager loadTransactions(ofType..) test
    Updated TestUtils to store 3 transactions of type income

--- baconTests/StorageManagerTests.swift
+++ baconTests/StorageManagerTests.swift
@@ -26,11 +26,13 @@ class StorageManagerTests: XCTestCase {
         XCTAssertNoThrow(try database.saveTransaction(TestUtils.validTransactionExpenditure01))
         XCTAssertNoThrow(try database.saveTransaction(TestUtils.validTransactionExpenditure02))
         XCTAssertEqual(database.getNumberOfTransactionsInDatabase(), 2)
+
         // swiftlint:enable force_try
     }
 
     func test_clearTransactionDatabase() {
         // swiftlint:disable force_try
+
         let database = try! StorageManager()
         // If database is empty, save a transaction
         if database.getNumberOfTransactionsInDatabase() == 0 {
@@ -39,21 +41,63 @@ class StorageManagerTests: XCTestCase {
         XCTAssertTrue(database.getNumberOfTransactionsInDatabase() > 0)
         XCTAssertNoThrow(try database.clearTransactionDatabase())
         XCTAssertTrue(database.getNumberOfTransactionsInDatabase() == 0)
+
         // swiftlint:enable force_try
     }
 
+    // This Test case might need to be updated when storage deals with transaction ids
     func test_saveTransaction() {
         // swiftlint:disable force_try
+
         let database = try! StorageManager()
         try! database.clearTransactionDatabase()
-        let transaction = try! Transaction(date: Date(),
-                                           type: .expenditure,
-                                           frequency: TransactionFrequency(nature: .oneTime),
-                                           category: .bills,
-                                           amount: 1)
-        XCTAssertNoThrow(try database.saveTransaction(transaction))
+        XCTAssertEqual(database.getNumberOfTransactionsInDatabase(), 0)
+        XCTAssertNoThrow(try database.saveTransaction(TestUtils.validTransactionExpenditure01))
+        // Load the transaction out of database and check if its the one that was saved
         let loadedTransaction = try! database.loadTransactions(ofType: .expenditure, limit: 1)
-        XCTAssertEqual(transaction, loadedTransaction.first)
+        XCTAssertEqual(TestUtils.validTransactionExpenditure01, loadedTransaction.first)
+
+        // swiftlint:enable force_try
+    }
+
+    func test_loadTransactions_OfType() {
+        // swiftlint:disable force_try
+
+        let database = try! StorageManager()
+        // Clear database
+        try! database.clearTransactionDatabase()
+        XCTAssertEqual(database.getNumberOfTransactionsInDatabase(), 0)
+        // Save 3 transactions of type expenditure
+        let expenditureTransactions = [TestUtils.validTransactionExpenditure03,
+                                       TestUtils.validTransactionExpenditure02,
+                                       TestUtils.validTransactionExpenditure01]
+        XCTAssertNoThrow(try database.saveTransaction(expenditureTransactions[2]))
+        XCTAssertNoThrow(try database.saveTransaction(expenditureTransactions[1]))
+        XCTAssertNoThrow(try database.saveTransaction(expenditureTransactions[0]))
+        let loadedExpenditureTransaction = try! database.loadTransactions(ofType: .expenditure, limit: 5)
+        XCTAssertEqual(loadedExpenditureTransaction.count, 3)
+        // Check that the transactions loaded out are equal and in reverse chronological order
+        XCTAssertEqual(loadedExpenditureTransaction, expenditureTransactions)
+
+        // Clear database
+        try! database.clearTransactionDatabase()
+        XCTAssertEqual(database.getNumberOfTransactionsInDatabase(), 0)
+        // Save 3 transactions of type income
+        let incomeTransactions = [TestUtils.validTransactionIncome03,
+                                  TestUtils.validTransactionIncome02,
+                                  TestUtils.validTransactionIncome01]
+        XCTAssertNoThrow(try database.saveTransaction(incomeTransactions[2]))
+        XCTAssertNoThrow(try database.saveTransaction(incomeTransactions[1]))
+        XCTAssertNoThrow(try database.saveTransaction(incomeTransactions[0]))
+        let loadedIncomeTransaction = try! database.loadTransactions(ofType: .income, limit: 5)
+        XCTAssertEqual(loadedIncomeTransaction.count, 3)
+        // Check that the transactions loaded out are equal and in reverse chronological order
+        XCTAssertEqual(loadedIncomeTransaction, incomeTransactions)
+
+        // Test limit
+        let loadedTransactions = try! database.loadTransactions(ofType: .income, limit: 1)
+        XCTAssertEqual(loadedTransactions.count, 1)
+
         // swiftlint:enable force_try
     }
 }
--- baconTests/TestUtils.swift
+++ baconTests/TestUtils.swift
@@ -20,6 +20,8 @@ class TestUtils {
     //     - The field that is constant
     //     - The number indicates the ordering of the date in the transaction
     //       (Chronological order)
+    //     * Note that excluding date and the constant field, the rest of the
+    //       transaction properties should differ from each other.
     // --------------------------------------------------
 
     // TRANSACTION - EXPENDITURE
@@ -43,6 +45,28 @@ class TestUtils {
                                                               repeats: 2),
                          category: .entertainment,
                          amount: 5.0)
+
+    // TRANSACTION - INCOME
+    static let validTransactionIncome01 =
+        try! Transaction(date: Date(timeIntervalSince1970: TimeInterval(0)),
+                         type: .income,
+                         frequency: try! TransactionFrequency(nature: .oneTime),
+                         category: .food,
+                         amount: 25.50)
+    static let validTransactionIncome02 =
+        try! Transaction(date: Date(timeIntervalSince1970: TimeInterval(1000)),
+                         type: .income,
+                         frequency: try! TransactionFrequency(nature: .recurring,
+                                                              interval: TransactionFrequencyInterval.monthly,
+                                                              repeats: 3),
+                         category: .food,
+                         amount: 12.80)
+    static let validTransactionIncome03 =
+        try! Transaction(date: Date(timeIntervalSince1970: TimeInterval(2000)),
+                         type: .income,
+                         frequency: try! TransactionFrequency(nature: .oneTime),
+                         category: .bills,
+                         amount: 1)
 
     // swiftlint:enable force_try
 }
