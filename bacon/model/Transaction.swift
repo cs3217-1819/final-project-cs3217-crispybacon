2162385e09bc6191a59e1188fde1ba93a9efdaa3
Fabian <fabianterh@gmail.com>
2019-03-28 13:21:36 +0800

    Conform Transaction to Observable

--- bacon/model/Transaction.swift
+++ bacon/model/Transaction.swift
@@ -8,14 +8,29 @@
 
 import Foundation
 
-class Transaction: Codable {
+/// Represents a mutable transaction.
+class Transaction: Codable, Observable {
 
-    var date: Date
-    var type: TransactionType
-    var frequency: TransactionFrequency
-    var category: TransactionCategory
-    var amount: Decimal
-    var description: String
+    private var _date: Date
+    private var _type: TransactionType
+    private var _frequency: TransactionFrequency
+    private var _category: TransactionCategory
+    private var _amount: Decimal
+    private var _description: String
+
+    var observers: [Observer] = []
+
+    // See: https://developer.apple.com/documentation/foundation/archives_and_serialization/encoding_and_decoding_custom_types
+    // We exclude the "observers" property from being encoded/decoded,
+    // since that information should not be persistent across sessions.
+    private enum CodingKeys: String, CodingKey {
+        case _date
+        case _type
+        case _frequency
+        case _category
+        case _amount
+        case _description
+    }
 
     /// Creates a Transaction instance.
     /// - Parameters:
@@ -43,12 +58,92 @@ class Transaction: Codable {
             throw InitializationError(message: "`amount` must be greater than 0")
         }
 
-        self.date = date
-        self.type = type
-        self.frequency = frequency
-        self.category = category
-        self.amount = amount
-        self.description = description
+        _date = date
+        _type = type
+        _frequency = frequency
+        _category = category
+        _amount = amount
+        _description = description
+    }
+
+    /// Notifies all observers of changes to self.
+    /// This should be called after any mutation to a Transaction instance.
+    private func notifyObserversOfSelf() {
+        log.info("Notifying observers of new self")
+        notifyObservers(self)
+    }
+
+    // Public computed properties
+    var date: Date {
+        get {
+            return _date
+        }
+
+        set(newDate) {
+            log.info("Setting date=\(newDate)")
+            _date = newDate
+            notifyObserversOfSelf()
+        }
+    }
+
+    var type: TransactionType {
+        get {
+            return _type
+        }
+
+        set(newType) {
+            log.info("Setting type=\(newType)")
+            _type = newType
+            notifyObserversOfSelf()
+        }
+    }
+
+    var frequency: TransactionFrequency {
+        get {
+            return _frequency
+        }
+
+        set(newFrequency) {
+            log.info("Setting frequency=\(newFrequency)")
+            _frequency = newFrequency
+            notifyObserversOfSelf()
+        }
+    }
+
+    var category: TransactionCategory {
+        get {
+            return _category
+        }
+
+        set(newCategory) {
+            log.info("Setting category=\(newCategory)")
+            _category = newCategory
+            notifyObserversOfSelf()
+        }
+    }
+
+    var amount: Decimal {
+        get {
+            return _amount
+        }
+
+        set(newAmount) {
+            log.info("Setting amount=\(newAmount)")
+            _amount = newAmount
+            notifyObserversOfSelf()
+        }
+    }
+
+    var description: String {
+        get {
+            return _description
+        }
+
+        set(newDescription) {
+            log.info("Setting description=\(newDescription)")
+            _description = newDescription
+            notifyObserversOfSelf()
+        }
     }
 
 }
