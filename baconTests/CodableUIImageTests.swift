3bd95d105e99443d796883a59b9b9fd87bdbdaf8
Fabian <fabianterh@gmail.com>
2019-04-02 15:43:42 +0800

    Add tests for CodableUIImage

---
+++ baconTests/CodableUIImageTests.swift
@@ -0,0 +1,48 @@
+//
+//  CodableUIImageTests.swift
+//  baconTests
+//
+//  Created by Fabian Terh on 2/4/19.
+//  Copyright © 2019 nus.CS3217. All rights reserved.
+//
+
+import XCTest
+@testable import bacon
+
+class CodableUIImageTests: XCTestCase {
+    // swiftlint:disable force_try
+    // swiftlint: disable force_unwrapping
+
+    func test_encode_decode_jpg() {
+        let testImage = UIImage(named: "red-heart-jpg")!
+
+        let encoder = JSONEncoder()
+        let encodedImageData = try! encoder.encode(CodableUIImage(testImage))
+
+        let decoder = JSONDecoder()
+        let decodedImage = try! decoder.decode(CodableUIImage.self, from: encodedImageData)
+
+        let reconstructedImage = decodedImage.image
+        // testImage.isEqual(reconstructedImage) does not work (returns false)
+        // We can't use == either (see: https://developer.apple.com/documentation/uikit/uiimage)
+        XCTAssertEqual(testImage.pngData()?.base64EncodedString(), reconstructedImage.pngData()?.base64EncodedString())
+    }
+
+    func test_encode_decode_png() {
+        let testImage = UIImage(named: "red-heart-png")!
+
+        let encoder = JSONEncoder()
+        let encodedImageData = try! encoder.encode(CodableUIImage(testImage))
+
+        let decoder = JSONDecoder()
+        let decodedImage = try! decoder.decode(CodableUIImage.self, from: encodedImageData)
+
+        let reconstructedImage = decodedImage.image
+        // testImage.isEqual(reconstructedImage) does not work (returns false)
+        // We can't use == either (see: https://developer.apple.com/documentation/uikit/uiimage)
+        XCTAssertEqual(testImage.pngData()?.base64EncodedString(), reconstructedImage.pngData()?.base64EncodedString())
+    }
+
+    // swiftlint:enable force_try
+    // swiftlint:enable force_unwrapping
+}
