27ce113cd0764e5d8456c9081926c5c2dc292c1a
Fabian <fabianterh@gmail.com>
2019-04-03 14:59:26 +0800

    Conform CodableUIImage to Hashable

--- bacon/commons/CodableUIImage.swift
+++ bacon/commons/CodableUIImage.swift
@@ -40,7 +40,7 @@ extension UIImage: Equatable {
 
 // MARK: CodableUIImage
 /// Codable and Equatable wrapper around CLLocation.
-struct CodableUIImage: Codable, Equatable {
+struct CodableUIImage: Codable, Equatable, Hashable {
 
     let image: UIImage
 
