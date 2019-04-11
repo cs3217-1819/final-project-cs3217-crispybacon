3a8c6ab6b67250ec08a4220265d366c13cd7a2f8
Fabian <fabianterh@gmail.com>
2019-04-11 21:43:13 +0800

    Add persistent storage to TagManager

--- bacon/model/Tags/TagManager.swift
+++ bacon/model/Tags/TagManager.swift
@@ -9,13 +9,70 @@
 import Foundation
 
 // MARK: TagManager: TagManagerInterface
-class TagManager: TagManagerInterface {
+class TagManager: Codable, TagManagerInterface {
+
+    // Support persistence
+    private static let saveFileName = "TagManager"
+    private static let errorLoadData = "Loading of TagManager data failed"
+    private static var cachedPersistentTagManager: TagManager?
+    private var isPersistent: Bool
 
     // Set of all Tags (both parent and child Tags). Use for membership check.
     private var allTags: Set<Tag> = []
 
     // Map of parent Tags to child Tags. Use for parent-child association check.
     private var parentChildMap: [Tag: Set<Tag>] = [:]
+
+    /// Creates and returns a TagManager object.
+    /// - Note: A persistent TagManager is implemented as a singleton, and thus will never be outdated.
+    /// - Parameter withPersistence: If `true`, changes to TagManager data (e.g. add/remove Tags) will
+    ///     automatically be saved to storage. The TagManager object returned will also be pre-loaded
+    ///     with pre-existing data. If `false`, the TagManager object returned will only manage Tags in memory.
+    static func create(withPersistence: Bool) -> TagManager {
+        log.info("Creating TagManager withPersistence=\(withPersistence).")
+
+        // Without persistence
+        if !withPersistence {
+            log.info("Returning non-persistent instance of TagManager.")
+            return TagManager(withPersistence: false)
+        }
+
+        // With persistence: if a persistent TagManager has previously been instantiated,
+        // we return it. Otherwise, we create a new instance and cache it.
+        if let tagManager = TagManager.cachedPersistentTagManager {
+            log.info("Returning cached instance of persistent TagManager.")
+            return tagManager
+        }
+
+        log.info("No cached instance of persistent TagManager found. Instantiating TagManager.")
+        let fsm = FileStorageManager()
+        let tagManager: TagManager
+
+        do {
+            log.info("Reading TagManager data from storage.")
+            tagManager = try fsm.readFromJson(TagManager.self, file: TagManager.saveFileName)
+            log.info("TagManager reconstructed from storage data")
+        } catch {
+            // This could happen if a persistent TagManager is instantiated for the first time ever
+            log.warning("\(TagManager.errorLoadData). Instantiating a new instance.")
+            tagManager = TagManager(withPersistence: true)
+            TagManager.cachedPersistentTagManager = tagManager
+            return tagManager
+        }
+
+        log.info("Caching TagManager for future returns.")
+        TagManager.cachedPersistentTagManager = tagManager
+        return tagManager
+    }
+
+    // Private init to disallow direct instantiation: we want to adopt a semi-singleton design pattern.
+    // When TagManager is instantiated in non-persistent mode (e.g. for unit testing),
+    // we can create as many instances as possible.
+    // However, in normal operation (persistent mode), we want to allow only 1 instance,
+    // to avoid multiple working stores of tag data.
+    private init(withPersistence: Bool) {
+        isPersistent = withPersistence
+    }
 
     func addChildTag(_ child: String, to parent: String) throws {
         let childTag = Tag(child, parent: parent)
@@ -112,6 +169,22 @@ class TagManager: TagManagerInterface {
 
 }
 
+// MARK: TagManager: persistence
+extension TagManager {
+
+    /// Saves the current state of the TagManager object to storage.
+    private func save() {
+        do {
+            log.info("Saving TagManager to storage.")
+            let fsm = FileStorageManager()
+            try fsm.writeAsJson(data: self, as: TagManager.saveFileName)
+        } catch {
+            log.error("Error encountered: \(error)")
+        }
+    }
+
+}
+
 // MARK: TagManager private utility methods
 extension TagManager {
 
@@ -140,6 +213,10 @@ extension TagManager {
             // Add to allTags
             allTags.insert(tag)
         }
+
+        if isPersistent {
+            save()
+        }
     }
 
     /// Removes multiple Tags from both the `allTags` set and `parentChildMap` dictionary.
@@ -166,6 +243,10 @@ extension TagManager {
 
             // Remove from allTags
             allTags.remove(tag)
+        }
+
+        if isPersistent {
+            save()
         }
     }
 
