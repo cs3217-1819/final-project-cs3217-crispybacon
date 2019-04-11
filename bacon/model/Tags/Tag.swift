9c03ae6a66b3224a72be91f08d176c59ad62fd18
Fabian <fabianterh@gmail.com>
2019-04-11 11:39:07 +0800

    Update Tag struct
    * Add documentation
    * Add convenience computed properties

--- bacon/model/Tags/Tag.swift
+++ bacon/model/Tags/Tag.swift
@@ -8,18 +8,33 @@
 
 import Foundation
 
+/// Tag ADT to represent a transaction tag.
+/// A Tag has a value, and an optional parent value.
+/// A Tag must be either a parent or child Tag.
 struct Tag: Codable, Equatable, Hashable {
 
     let value: String
     let parent: String?
 
+    /// Initializes a parent Tag.
     init(_ value: String) {
         self.init(value, parent: nil)
     }
 
+    /// Initializes a child Tag.
     init(_ value: String, parent: String?) {
         self.value = value
         self.parent = parent
+    }
+
+    /// Convenience computed property to represent whether a Tag is a child Tag.
+    var isChild: Bool {
+        return parent != nil
+    }
+
+    /// Convenience computed property to represent whether a Tag is a parent Tag.
+    var isParent: Bool {
+        return !isChild
     }
 
 }
