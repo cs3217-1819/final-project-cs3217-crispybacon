ca861106c30ff4f9c96b13e4856cc5fb809c6387
kumuwu <e0203066@u.nus.edu>
2019-04-19 22:09:26 +0800

    set up storyboard and VCs

---
+++ bacon/controller/LocationAnalysisViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationAnalysisViewController.swift
+//  bacon
+//
+//  Created by Lizhi Zhang on 19/4/19.
+//  Copyright © 2019 nus.CS3217. All rights reserved.
+//
+
+import UIKit
+
+class LocationAnalysisViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
